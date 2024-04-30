65f962121dcbf55607a0fb5243a09e27fba09c0f
zyang <zyangmath@gmail.com>
2024-04-30 13:36:17 +0800

    refactor: cleanup code

--- calibrate.py
+++ calibrate.py
@@ -1,25 +1,20 @@
 #!/usr/bin/env python3
 # coding: utf8
 
-from driver import StepperMotor
-import RPi.GPIO as GPIO
+import driver
 import json
-import time
 
 
 def calibrate(output_fn):
-    motor_x = StepperMotor(5, 3, 4)
-    motor_y = StepperMotor(8, 6, 7)
-    x_data = motor_x.calibrate(500)
-    y_data = motor_y.calibrate(500)
-    with open(output_fn, "w", encoding="utf8") as f:
-        json.dump({"motor_x": x_data, "motor_y": y_data}, f, indent=2)
+    with driver.GpioManager() as _:
+        motor_x = driver.BoundedStepperMotor(5, 3, 4, 6, 7)
+        motor_y = driver.BoundedStepperMotor(12, 10, 11, 8, 9)
+        freq = 1000
+        x_data = motor_x.calibrate(freq)
+        y_data = motor_y.calibrate(freq)
+        with open(output_fn, "w", encoding="utf8") as f:
+            json.dump({"motor_x": x_data, "motor_y": y_data}, f, indent=2)
 
-
-def main():
-    GPIO.setmode(GPIO.BCM)
-    calibrate("motor_spec.json")
-    GPIO.cleanup()
 
 if __name__ == "__main__":
-    main()
+    calibrate()
--- driver.py
+++ driver.py
@@ -2,6 +2,18 @@
 
 import time
 import RPi.GPIO as GPIO
+import logging
+logging.basicConfig(level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] -- %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+class GpioManager(object):
+    def __enter__(self):
+        GPIO.setmode(GPIO.BCM)
+        return self
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        GPIO.cleanup()
+        return False
 
 class StepperMotor(object):
     def __init__(self, pin_en, pin_dir, pin_stp):
@@ -47,3 +59,64 @@ class StepperMotor(object):
 
     def hold(self):
         GPIO.output(self.pins[0], GPIO.HIGH)
+
+class BoundedStepperMotor(object):
+    def __init__(self, pin_en, pin_dir, pin_stp, pin_b0, pin_b1,
+                 freq=1000, dc=0.5):
+        self.pins = [pin_en, pin_dir, pin_stp]
+        self.bounds = [pin_b0, pin_b1]
+        self.default_freq = freq
+        self.default_dc = dc
+        self.reset()
+
+    def calibrate(self, freq=1000):
+         # a very large duration to ensure reach the boundary.
+        trial_duration = 3600 # an hour
+        self.backward(trial_duration, freq)
+        t0 = time.clock_gettime_ns(time.CLOCK_MONOTONIC) * 1e-9
+        self.forward(trial_duration, freq)
+        t1 = time.clock_gettime_ns(time.CLOCK_MONOTONIC) * 1e-9
+        ans = input("Does the full move goes forward (Y) or backward (N)?")
+        direction = True if ans.lower().startswith("y") else False
+        return {"clockwise": direction, "freq": freq, "time": t1 - t0}
+
+    def reset(self):
+        for p in self.pins:
+            GPIO.setup(p, GPIO.OUT, initial=GPIO.LOW)
+        for p in self.bounds:
+            GPIO.setup(p, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    def drive(self, duration, freq=None, dc=None, clockwise=True):
+        real_freq = self.default_freq if freq is None else freq
+        real_dc = dc if dc is not None else self.default_dc
+        GPIO.output(self.pins[0], GPIO.HIGH)
+        GPIO.output(self.pins[1], GPIO.HIGH if clockwise else GPIO.LOW)
+        p = GPIO.PWM(self.pins[2], real_freq)
+        d = self.bounds[1] if clockwise else self.bounds[0]    
+        p.start(real_dc * 100) # GPIO.PWM use dc from 0 to 100
+        GPIO.wait_for_edge(d, GPIO.RISING, timeout=duration*1000)
+        p.stop()
+
+    def forward(self, duration=3600, freq=None, dc=None):
+        self.drive(duration, freq, dc, True)
+
+    def backward(self, duration=3600, freq=None, dc=None):
+        self.drive(duration, freq, dc, False)
+
+    def release(self):
+        GPIO.output(self.pins[0], GPIO.LOW)
+
+    def hold(self):
+        GPIO.output(self.pins[0], GPIO.HIGH)
+
+def main():
+    with GpioManager() as _:
+        motor_x = BoundedStepperMotor(5, 3, 4, 6, 7, freq=1000)
+        print(motor_x.calibrate())
+        for _ in range(4):
+            motor_x.forward(5)
+            motor_x.backward(5)
+
+
+if __name__ == "__main__":
+    main()
